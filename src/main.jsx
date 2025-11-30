448794a6cfb3f419eb3fc3959161af93f6d0580c
Julio Perales Muñoz <julio.epila@gmail.com>
2025-11-30 13:22:25 +0100

    Forzar importación de CSS standard

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'  // <--- ¡ESTA LÍNEA ES LA QUE PINTA LA WEB!
+import './index.css' // <--- ESTA LÍNEA ES OBLIGATORIA
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
